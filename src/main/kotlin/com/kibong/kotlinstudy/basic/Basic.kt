b24d1d0910440eb576ab37679775ce41bad4332e
kibongcorders <kkp02052@gmail.com>
2024-05-28 00:23:19 +0900

    study : 데이터 타입

--- src/main/kotlin/com/kibong/kotlinstudy/basic/Basic.kt
+++ src/main/kotlin/com/kibong/kotlinstudy/basic/Basic.kt
@@ -4,6 +4,7 @@ fun main() {
     //main 함수로 코드를 실행할 수 있다.
     println("Hello, Kotlin!")
     valAndVar()
+    dataType()
 }
 
 // Kotlin에서 val 키워드를 통해 불변 변수를 선언
@@ -23,13 +24,31 @@ fun valAndVar() {
 
 fun dataType() {
 
-
     val int: Int = 1
     val long: Long = 1L
     val float: Float = 1.0f
     val double: Double = 1.0
     val char: Char = 'A'
     val string: String = "Hello, Kotlin!"
+    val compareString : String = "Hello, Kotlin!"
     val boolean: Boolean = true
+
+    val noType = 1L;
+
+    data class User(val name: String, val age: Int)
+
+    val user = User("Kibong", 27)
+    val user1 = User("Kibong", 27)
+    val user2 = User("Kibong", 28)
+
+    //타입 비교 방법
+    println(noType::class.simpleName)
+    println(int.toLong() == long)
+    println(float.toDouble() == double)
+    println(string == compareString)
+    println(user == user1)
+    println(user == user2)
+
+
 
 }
